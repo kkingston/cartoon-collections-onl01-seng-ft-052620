9e6b4da1259be4db2a4e67a509ee683e26e415f5
Kayla Kingston <kkingston09@gmail.com>
2020-06-08 15:38:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(veggies)
 end
 
 def long_planeteer_calls(calls)
-  {|word| word.any?(word.length > 4)}
+  word.any? {|word| word.any?(word.length > 4)}
 end
 
 def find_the_cheese(array)
